72f962098d540c602a69d202d424dcb3207e828d
jdamick <jeffreydamick@gmail.com>
2012-03-27 15:23:28 -0400

    added psring repo

---
+++ metrics/test/java/biz/neustar/service/metrics/DaemonTest.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright 2000-2012 NeuStar, Inc. All rights reserved.
+ * NeuStar, the Neustar logo and related names and logos are registered
+ * trademarks, service marks or tradenames of NeuStar, Inc. All other
+ * product names, company names, marks, logos and symbols may be trademarks
+ * of their respective owners.
+ */
+
+package biz.neustar.service.metrics;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DaemonTest {
+
+    @Test
+    public void testStartup() {
+        
+    }
+}
