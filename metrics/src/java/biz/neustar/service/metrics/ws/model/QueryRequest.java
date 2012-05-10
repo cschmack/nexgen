84869de47404337a457456bf0130556b92ac2a4e
John Liberty <libjackjl@gmail.com>
2012-05-09 23:45:02 -0400

    first pass: adding time calculations for QueryRequest

--- metrics/src/java/biz/neustar/service/metrics/ws/model/QueryRequest.java
+++ metrics/src/java/biz/neustar/service/metrics/ws/model/QueryRequest.java
@@ -9,11 +9,41 @@
 package biz.neustar.service.metrics.ws.model;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.google.common.base.Objects;
+import com.google.common.base.Strings;
 
 public class QueryRequest {
-    
+	
+	@JsonIgnoreProperties
+	class StartEndTimeMillis {
+		private long start;
+		private long end;
+		
+		public StartEndTimeMillis(long start, long end) {
+			this.start = start;
+			this.end = end;
+		}
+		
+		public long getStart() {
+			return start;
+		}
+		
+		public long getEnd() {
+			return end;
+		}
+	}
+ 
+	public static final String TS_REGEX = "^(-)(\\d+)([smhDM])$";
+	public static final String TE_REGEX = "^(\\d+)([smhDM])$";
+
     private List<String> contexts;
     private String ts;
     private String te;
@@ -21,7 +51,69 @@ public class QueryRequest {
     private List<String> stats;
     private boolean raw = false;
     
+    @JsonIgnore
+    public StartEndTimeMillis getStartEndTimeMillis() {
+    	DateTime dt = getStartDateTime();
+    	
+    	return new StartEndTimeMillis(dt.getMillis(), getEndDateTime(dt).getMillis());
+    }
     
+    @JsonIgnore
+    public DateTime getStartDateTime() {
+		DateTime startTime = DateTime.now();
+		// try and calculate
+		if (!Strings.isNullOrEmpty(ts)) {
+			Matcher matcher = Pattern.compile(QueryRequest.TS_REGEX).matcher(ts);
+			if (matcher.matches()) {
+				boolean neg = matcher.group(1).equals("-");
+				int val = Integer.parseInt(matcher.group(2));
+				char suffix = matcher.group(3).charAt(0);
+				// smhDM
+				if (suffix == 's') {
+					startTime = neg ? startTime.minusSeconds(val) : startTime.plusSeconds(val);
+				} else if (suffix == 'm') {
+					startTime = neg ? startTime.minusMinutes(val) : startTime.plusMinutes(val);
+				} else if (suffix == 'h') {
+					startTime = neg ? startTime.minusHours(val) : startTime.plusHours(val);
+				} else if (suffix == 'D') {
+					startTime = neg ? startTime.minusDays(val) : startTime.plusDays(val);
+				} else if (suffix == 'M') {
+					startTime = neg ? startTime.minusMonths(val) : startTime.plusMonths(val);
+				}
+			} else {
+				startTime = DateTime.parse(ts, ISODateTimeFormat.localDateOptionalTimeParser());
+			}
+		}
+		return startTime;
+    }
+    
+    @JsonIgnore
+    public DateTime getEndDateTime(DateTime startDateTime) {
+    	DateTime endTime = DateTime.now();
+    
+		if (!Strings.isNullOrEmpty(te)) {
+			Matcher matcher = Pattern.compile(QueryRequest.TE_REGEX).matcher(te);
+			if (matcher.matches()) {
+				int val = Integer.parseInt(matcher.group(1));
+				char suffix = matcher.group(2).charAt(0);
+				// smhDM
+				if (suffix == 's') {
+					endTime = startDateTime.plusSeconds(val);
+				} else if (suffix == 'm') {
+					endTime = startDateTime.plusMinutes(val);
+				} else if (suffix == 'h') {
+					endTime = startDateTime.plusHours(val);
+				} else if (suffix == 'D') {
+					endTime = startDateTime.plusDays(val);
+				} else if (suffix == 'M') {
+					endTime = startDateTime.plusMonths(val);
+				}
+			} else {
+				endTime = DateTime.parse(te, ISODateTimeFormat.localDateOptionalTimeParser());
+			}
+		}
+    	return endTime;
+    }
     
     public List<String> getContexts() {
         return contexts;
@@ -34,6 +126,7 @@ public class QueryRequest {
     }
     public void setTs(String ts) {
         this.ts = ts;
+        
     }
     public String getTe() {
         return te;
--- metrics/src/java/biz/neustar/service/metrics/ws/model/validation/QueryRequestValidator.java
+++ metrics/src/java/biz/neustar/service/metrics/ws/model/validation/QueryRequestValidator.java
@@ -6,15 +6,13 @@ import java.util.regex.Pattern;
 import javax.xml.bind.DatatypeConverter;
 
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 import biz.neustar.service.metrics.ws.model.QueryRequest;
 
 public class QueryRequestValidator implements Validator {
 	
-	public static final String TS_REGEX = "^-\\d+[smhDM]$";
-	public static final String TE_REGEX = "^\\d+[smhDM]$";
-
 	@Override
 	public boolean supports(Class<?> clazz) {
 		return QueryRequest.class.equals(clazz);
@@ -23,12 +21,19 @@ public class QueryRequestValidator implements Validator {
 	@Override
 	public void validate(Object obj, Errors e) {
 		QueryRequest queryRequest = (QueryRequest)obj;
+		
+		// context is required
+		ValidationUtils.rejectIfEmptyOrWhitespace(e, "contexts", "contexts.empty");
+		if (queryRequest.getContexts().size() == 0) {
+			e.rejectValue("contexts","contexts.empty", "contexts is required");
+		}
+		
 		// ts is required, but can be empty
 		if (queryRequest.getTs() == null) {
 			e.rejectValue("ts","ts.null", "ts is required");
 		} else if (queryRequest.getTs().trim().length() != 0) {
 			// look for the relative pattern first, and then try to parse as timestamp
-			if (!Pattern.compile(TS_REGEX).matcher(queryRequest.getTs()).matches()) {
+			if (!Pattern.compile(QueryRequest.TS_REGEX).matcher(queryRequest.getTs()).matches()) {
 	            try {
 	                @SuppressWarnings("unused")
 	                Calendar c = DatatypeConverter.parseDateTime(queryRequest.getTs());
@@ -39,7 +44,7 @@ public class QueryRequestValidator implements Validator {
 			}
 		}
 		if (queryRequest.getTe() != null) {
-			if (!Pattern.compile(TE_REGEX).matcher(queryRequest.getTe()).matches()) {
+			if (!Pattern.compile(QueryRequest.TE_REGEX).matcher(queryRequest.getTe()).matches()) {
 	            try {
 	                @SuppressWarnings("unused")
 	                Calendar c = DatatypeConverter.parseDateTime(queryRequest.getTe());
